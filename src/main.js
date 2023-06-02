ab724a6e9b13bea8e57ff9b6aeb4dc2354e9988f
MONISH-JODHA <104707700+MONISH-JODHA@users.noreply.github.com>
2023-06-02 10:37:31 +0000

    basic commands added to commit

--- src/main.js
+++ src/main.js
@@ -3,13 +3,36 @@ document.addEventListener("DOMContentLoaded", function(){
     const display= document.getElementById("calc-display");
     const buttons= document.querySelectorAll(".btn");
     let currentvalue="";
+    function evaluate(){
+        const newvalue=currentvalue.replace("×","*")
+            .replace("÷","/").replace("%","*0.01");
+        currentvalue=newvalue;
+        const result=eval(currentvalue);
+        currentvalue=result.toString();
+        display.value=currentvalue;
+    }
+
+   
+    
        for (let i=0; i < buttons.length; i++){
         const button=buttons[i];
         button.addEventListener("click", function(){
             console.log("button clicked",button.innerText);
-            const value=button.innerText;
-            currentvalue+=value;
-            display.value=currentvalue;
+            const valuee=button.innerText;
+
+            
+            if (valuee== "AC"){
+                currentvalue="";
+                display.value=currentvalue;
+            }
+            else if(valuee=="="){
+                evaluate();
+            }
+            else{
+                currentvalue+=valuee;
+                display.value=currentvalue;
+            }    
+            
             
          })
     }
