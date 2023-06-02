7c7af56402f8c5e093dbf1e081ec9f604794d600
MONISH-JODHA <104707700+MONISH-JODHA@users.noreply.github.com>
2023-06-02 10:21:01 +0000

    added basic functionality

--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
 document.addEventListener("DOMContentLoaded", function(){
+    console.log("document is ready");
     const display= document.getElementById("calc-display");
-    const buttons= document.querySelector(".btn");
-   
+    const buttons= document.querySelectorAll(".btn");
+    let currentvalue="";
        for (let i=0; i < buttons.length; i++){
         const button=buttons[i];
         button.addEventListener("click", function(){
-        console.log("button ",button.innerText)
+            console.log("button clicked",button.innerText);
+            const value=button.innerText;
+            currentvalue+=value;
+            display.value=currentvalue;
+            
          })
     }
    
